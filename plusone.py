2f258316267729b7307424e77c806f2af9c2a65f
Madhumitha Venkatesan <71954642+vmadhuuu@users.noreply.github.com>
2021-08-08 20:05:28 +0800

    Add files via upload

---
+++ plusone.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits:List[int]):
+        length = len(digits) - 1
+        
+        while digits[length] ==9:
+            digits[length] = 0
+            length -=1
+        
+        if (length < 0):
+            digits = [1] + digits
+        else:
+            digits[length] += 1
+        return digits 
+
